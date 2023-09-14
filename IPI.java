c8e19d4926613fee4edc0b8e6a96ad91adaf3024
Rafael Manggini <115665155+raferasss@users.noreply.github.com>
2023-09-14 20:27:43 -0300

    Corrigindo Valores

--- IPI.java
+++ IPI.java
@@ -2,7 +2,7 @@ public class IPI {
     double aliquota = 0.25;
 
     double calcular_IPI(Produto p) {
-        double valor = p.valor + (aliquota*p.valor);
+        double valor =(aliquota*p.valor);
         return valor;
     }
 }
