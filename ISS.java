5db00851bf510cb255e08b0900e0376552518fb4
Rafael Manggini <115665155+raferasss@users.noreply.github.com>
2023-09-14 20:26:48 -0300

    Corrigindo Valores

--- ISS.java
+++ ISS.java
@@ -2,7 +2,7 @@ public class ISS {
     double aliquota = 0.046;
 
     double calcular_ISS(Servico s) {
-        double valor = s.valor+(s.valor*aliquota);
+        double valor = (s.valor*aliquota);
         return valor;
     }
 }
